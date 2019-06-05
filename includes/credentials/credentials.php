0ce991ede18c30ae56f0cbae52871cb414d3441d
RianBergen <rianb98@gmail.com>
2019-06-04 20:12:02 -0400

    Added Credentials Page

---
+++ includes/credentials/credentials.php
@@ -0,0 +1,35 @@
+<!-- This File Stores All Credentials For https://www.rianbergen.com/ -->
+<?php
+// Database Credentials
+define('DBHOST', 'hostname');
+define('DBPORT', 'port');
+define('DBNAME', 'database');
+define('DBUSER', 'user');
+define('DBPASS', 'password');
+
+// TinyMCE Credentials
+// Replace 'key' With Your TinyMCE Link (Which Includes The API Key)
+// Example: define('TINYMCE', 'https://cloud.tinymce.com/5/tinymce.min.js?apiKey=000000000000000000000000000000000000000000000000');
+define('TINYMCE', 'key');
+
+// Disqus Credentials
+// Replace 'key' With Your DISQUS Short Code (xxxxxxx.disqus.com)
+// Example: define('DISQUS', 'xxxxxxx');
+define('DISQUS', 'key');
+
+// Absolute URL
+// Replace 'absolute' With Your Websites Absolute URL
+// Example: define('URL', 'https://www.rianbergen.com/');
+define('URL', 'absolute');
+
+// HTML Values
+// This Fills Out The HTML Title And Description Tags
+define('HTMLTITLE', 'title');
+define('HTMLDESCRIPTION', 'description');
+
+// Titlebar Values
+// These Fill Out The Header And Footer Values Located At The Top And Bottom Of The Page
+define('TITLE', 'title');
+define('DESCRIPTION', 'description');
+define('COPYRIGHT', 'copyright');
+?>
